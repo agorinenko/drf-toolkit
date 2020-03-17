18703fa49cbfe26d9cccc32d65de4cc275736d71
anton <anton.gorinenko@gmail.com>
2020-03-17 14:41:02 +0300

    fix

--- drf_toolkit/decorators.py
+++ drf_toolkit/decorators.py
@@ -1,5 +1,6 @@
+from functools import wraps
+
 from power_dict.schema_validator import SchemaValidator
-from six import wraps
 from drf_toolkit.drf_utils import DrfUtils
 
 def validate_request(schema):
@@ -8,6 +9,7 @@ def validate_request(schema):
         def _wrapped_view(view, *args, **kwargs):
             request = view.request
             context = DrfUtils.get_request_parameters(request)
+
             context = SchemaValidator.validate(context, schema)
             kwargs['context'] = context
             return view_func(view, *args, **kwargs)
